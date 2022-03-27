ee13a28539a46c23ac22aae9fe5664cb0a0c6ebc
Weslen Mendes <weslenmends@gmail.com>
2022-03-27 10:14:44 -0300

    feature: implemented the functionality to return to home when clicking on the h1 tag of the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
 import "./style.scss";
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <header className="header">
-      <h1>Cineflex</h1>
+      <h1 onClick={() => navigate("/")}>Cineflex</h1>
     </header>
   );
 };
