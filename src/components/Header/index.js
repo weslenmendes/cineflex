a4530e47e9fb60db7c81fc3975390dc3daf5a4a0
Weslen Mendes <weslenmends@gmail.com>
2022-03-28 00:28:18 -0300

    feature: implemented a back button

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,24 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+
+import { ReactComponent as Logo } from "./../../assets/arrow-back-circle.svg";
 
 import "./style.scss";
 
 const Header = () => {
   const navigate = useNavigate();
+  const { pathname } = useLocation();
 
   return (
     <header className="header">
+      {pathname === "/" ? (
+        <></>
+      ) : (
+        <Logo
+          className="back-button"
+          fill="#e8833a"
+          onClick={() => navigate(-1)}
+        />
+      )}
       <h1 onClick={() => navigate("/")}>Cineflex</h1>
     </header>
   );
